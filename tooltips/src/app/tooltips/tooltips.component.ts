ce8e5652396d8270642789c01ca072abbf88b70d
Shoab Khan <khanshoab669@gmail.com>
2022-08-24 14:35:31 +0530

    Add files via upload

---
+++ tooltips/src/app/tooltips/tooltips.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tooltips',
+  templateUrl: './tooltips.component.html',
+  styleUrls: ['./tooltips.component.css']
+})
+export class TooltipsComponent implements OnInit {
+  showToolTip: boolean = false
+  @Input() toolTipMsg: string = ''
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  openToolTip(){
+    this.showToolTip = true
+  }
+
+  closeToolTip(e: any){
+    e.preventDefault
+    this.showToolTip = false
+  }
+
+}
